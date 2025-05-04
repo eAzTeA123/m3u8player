9ebdc0760ccc7a491428addae16192084c9939dc
eAzTeA123 <53313876+eAzTeA123@users.noreply.github.com>
2025-05-04 20:11:06 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
   const { url, ref } = req.query;
@@ -11,17 +11,13 @@ export default async function handler(req, res) {
     const decodedUrl = decodeURIComponent(url);
     const decodedRef = decodeURIComponent(ref);
 
-    console.log(`Abrufe Stream von URL: ${decodedUrl} mit Referer: ${decodedRef}`);
-
     const response = await fetch(decodedUrl, {
-      method: 'GET',
       headers: {
         'Referer': decodedRef,
+        'Origin': decodedRef,   // Dieser Header könnte wichtig sein, um CORS-Probleme zu verhindern
         'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
       }
     });
-
-    console.log('Response Status:', response.status);
 
     if (!response.ok) {
       return res.status(response.status).send('Stream konnte nicht geladen werden.');
@@ -30,7 +26,6 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
     response.body.pipe(res);
   } catch (err) {
-    console.error('Fehler beim Abrufen des Streams:', err);
     res.status(500).send('Fehler beim Abrufen des Streams.');
   }
 }
